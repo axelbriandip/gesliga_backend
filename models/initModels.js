0f9da5a5de26654a9b28ff1801314cece2412230
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-09-17 02:37:35 -0300

    player model add

--- models/initModels.js
+++ models/initModels.js
@@ -2,6 +2,7 @@
 const { User } = require('./user.model')
 const { Club } = require('./club.model')
 const { Request } = require('./request.model')
+const { Player } = require('./player.model')
 
 // relations
 const initModels = () => {
---
+++ models/player.model.js
@@ -0,0 +1,78 @@
+const { db, DataTypes } = require('../utils/db.util');
+
+// create model
+const Player = db.define('player', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    first_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    date_of_birth: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    nationality: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    position: {
+        type: DataTypes.ENUM('Arquero', 'Defensor', 'Mediocampista', 'Delantero'),
+        allowNull: false
+    },
+    preferred_foot: {
+        type: DataTypes.ENUM('Zurdo', 'Derecho', 'Ambidiestro'),
+        allowNull: false
+    },
+    gender: {
+        type: DataTypes.ENUM('Femenino', 'Masculino'),
+        allowNull: false
+    },
+    birth_state: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    contact_phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            is: /^[0-9]+$/ // Solo números
+        }
+    },
+    dni: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isNumeric: true
+        }
+    },
+    license_number: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    is_active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    pass_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'pass', // Nombre de la tabla relacionada
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true //add createdAt and updatedAt
+})
+
+// export
+module.exports = { Player };

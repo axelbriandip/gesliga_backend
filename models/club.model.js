0092d3e6925ed4099ddccad640efd8176209e1ce
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-09-25 02:30:31 -0300

    solved the problem of creating club

--- models/club.model.js
+++ models/club.model.js
@@ -9,77 +9,59 @@ const Club = db.define('club', {
         allowNull: false
     },
     full_name: {
-        type: DataTypes.STRING(100),
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true
     },
     short_name: {
-        type: DataTypes.STRING(50),
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true
     },
     abb_name: {
-        type: DataTypes.STRING(3),
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true
     },
     primary_color: {
         type: DataTypes.STRING,
         allowNull: false,
-        // validate: {
-        //     is: /^#[0-9A-F]{6}$/i // Validación para hexadecimal
-        // }
     },
     secondary_color: {
         type: DataTypes.STRING,
         allowNull: false,
-        // validate: {
-        //     is: /^#[0-9A-F]{6}$/i
-        // }
-    },    
+    },
     president_first_name: {
-        type: DataTypes.STRING(50),
-        allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     president_last_name: {
-        type: DataTypes.STRING(50),
-        allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     president_contact: {
-        type: DataTypes.STRING(20), // Limitar la longitud
-        allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     history: {
-        type: DataTypes.TEXT,
-        allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     instagram: {
         type: DataTypes.STRING,
         allowNull: false,
-        // validate: {
-        //     isUrl: true
-        // }
     },
     facebook: {
         type: DataTypes.STRING,
         allowNull: false,
-        // validate: {
-        //     isUrl: true
-        // }
     },
     whatsapp: {
         type: DataTypes.STRING,
         allowNull: false,
-        // validate: {
-        //     is: /^[0-9]+$/  // Valida que contenga solo números
-        // }
     },
     website: {
         type: DataTypes.STRING,
         allowNull: false,
-        // validate: {
-        //     isUrl: true
-        // }
     },
     is_active: {
         type: DataTypes.BOOLEAN,
